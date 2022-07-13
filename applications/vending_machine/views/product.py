f7ca5998ebdfc87caef4c39e40bcd99dfa9c3e71
Marsel <marselbeqiri.365@gmail.com>
2022-07-14 01:42:48 +0200

    feat(vending_machine): Product View

---
+++ applications/vending_machine/views/product.py
@@ -0,0 +1,11 @@
+from rest_framework.viewsets import ModelViewSet
+
+from applications.vending_machine.filters import ProductFilter
+from applications.vending_machine.models import Product
+from applications.vending_machine.serializers import ProductSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all().select_related('seller')
+    serializer_class = ProductSerializer
+    filterset_class = ProductFilter
